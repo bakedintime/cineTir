807ce9cde02bf052280ed05a45d693d2a65e63bb
Joel Cantoral <cs.joel.09@gmail.com>
2015-10-08 18:07:01 -0600

    Added functionality to show if a survey is open or close #6

--- cineTir/www/js/controllers.js
+++ cineTir/www/js/controllers.js
@@ -43,12 +43,12 @@ angular.module('starter.controllers', [])
 
 .controller('DashboardCtrl', function($scope) {
   $scope.surveys = [
-    { title: 'Viernes 15 oct', id: 1, date: '2015-10-15T10:00:00-00:00' },
-    { title: 'Martes 2 julio', id: 2, date: '2015-07-02T10:00:00-00:00' },
-    { title: 'Lunes 1 mayo', id: 3, date: '2015-05-01T10:00:00-00:00' },
-    { title: 'Miércoles 4 de febrero', id: 4, date: '2015-02-04T10:00:00-00:00' },
-    { title: 'Lunes 2 de febrero', id: 5, date: '2015-02-02T10:00:00-00:00' },
-    { title: 'Martes 1 de enero', id: 6, date: '2015-01-01T10:00:00-00:00' }
+    { title: 'Películas Viernes', id: 1, date: '2015-10-15T10:00:00-00:00', open: true },
+    { title: 'Películas Martes', id: 2, date: '2015-07-02T10:00:00-00:00', open: true },
+    { title: 'Películas Lunes', id: 3, date: '2015-05-01T10:00:00-00:00', open: true },
+    { title: 'Películas Miércoles', id: 4, date: '2015-02-04T10:00:00-00:00', open: false },
+    { title: 'Películas Lunes', id: 5, date: '2015-02-02T10:00:00-00:00', open: false },
+    { title: 'Películas Martes', id: 6, date: '2015-01-01T10:00:00-00:00', open: false }
   ];
 
   $scope.format = function(date){
